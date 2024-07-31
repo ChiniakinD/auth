1eea8b9d7fd8950e924be9907c0974a1f11057e3
ChiniakinD <pilotuiga@gmail.com>
2024-07-31 23:33:45 +0300

    feat(): Добавил подключение к БД. 1. Добавил подключение к liquibase. 2. Добавил создание таблиц roles, users и user_roles при помощи yml. 3. Внёс список ролей в таблицу roles. 4. Подключил необходимые зависимости.

---
+++ src/main/resources/liquibase/migration/V2__insert.sql
@@ -0,0 +1,9 @@
+insert into roles (role) values
+                                 ('USER'),
+                                 ('CREDIT_USER'),
+                                 ('OVERDRAFT_USER'),
+                                 ('DEAL_SUPERUSER'),
+                                 ('CONTRACTOR_RUS'),
+                                 ('CONTRACTOR_SUPERUSER'),
+                                 ('SUPERUSER'),
+                                 ('ADMIN');
